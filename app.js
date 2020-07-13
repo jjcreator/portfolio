4b08270e8bcd492bbfdba538669370c554b09f5d
Jakub Jaskuła <jjcreator8@gmail.com>
2020-07-13 10:24:35 +0200

    added modal

--- app.js
+++ app.js
@@ -1,91 +1,3 @@
-// Scroll container navigation
-
-const scrollContainer = document.querySelector("#scroll-wrapper");
-const bottomArrow = document.querySelector("#bottom");
-const topArrow = document.querySelector("#top");
-const linksArray = Array.from(document.querySelectorAll(".link"));
-let currentRow = 0;
-
-linksArray.forEach(link => {
-    link.addEventListener("click", ()=> {
-        currentRow = linksArray.indexOf(link);
-        scrollContainer.style.transition = "none";
-        changePosition();
-        setTimeout(()=> {
-            scrollContainer.style.transition = "ease-in 1s transform"
-        }, 1)
-    })
-});
-
-bottomArrow.addEventListener("click", ()=> {
-    changeRow("down");
-    changePosition();
-});
-
-topArrow.addEventListener("click", ()=> {
-    changeRow("up");
-    changePosition();
-});
-
-const changeRow = direction => {
-    switch(direction) {
-        case "up": currentRow > 0 ? currentRow--: null;
-        break;
-        case "down": currentRow < 3 ? currentRow++: null;
-        break;
-        default: break;
-    }
-}
-
-const changePosition = () => {
-    scrollContainer.style.transform = `translateY(-${currentRow * 100}vh)`
-}
-
-const mouseScroll = e => {
-        if(e.deltaY > 0) {
-            changeRow("down");
-            changePosition();
-        }
-        else {
-            changeRow("up");
-            changePosition();
-        }
-        window.removeEventListener("wheel", mouseScroll);
-        setTimeout(()=> {
-            window.addEventListener("wheel", mouseScroll);
-        }, 500)
-}
-
-window.addEventListener("wheel", mouseScroll);
-
-window.addEventListener("keydown", e => {
-    window.removeEventListener("wheel", mouseScroll);
-    let keyPressed = e.key;
-    switch(keyPressed) {
-        case "ArrowDown": 
-            changeRow("down");
-            changePosition();
-            break;
-        case "ArrowUp":
-            changeRow("up");
-            changePosition();
-            break;
-        default: break;
-    }
-});
-
-window.addEventListener("keyup", ()=> {
-    window.addEventListener("wheel", mouseScroll);
-});
-
-// Main page 
-
-const mainButton = document.querySelector(".main-button");
-mainButton.addEventListener("click", ()=> {
-    currentRow = 1;
-    changePosition();
-})
-
 // Language change 
 
 const flagsArray = document.querySelectorAll(".flag");
@@ -119,3 +31,28 @@ flagsArray.forEach(flag => {
 const submitButton = document.querySelector(".submit")
 
 submitButton.addEventListener("click", e => e.preventDefault())
+
+// Modal
+
+const modal = document.querySelector(".modal");
+const modalBackground = document.querySelector(".modal-background");
+modal.style.display = "none";
+modalBackground.style.display = "none";
+const modalExit = document.querySelector(".modal-exit");
+
+modalExit.addEventListener("click", ()=> {
+    modal.style.display = "none";
+    modalBackground.style.display = "none";
+});
+
+modalBackground.addEventListener("click", ()=> {
+    modal.style.display = "none";
+    modalBackground.style.display = "none";
+})
+
+const projectItems = document.querySelectorAll(".projects-grid-item");
+
+projectItems.forEach(item => item.addEventListener("click", ()=> {
+    modal.style.display = "flex";
+    modalBackground.style.display = "block";
+}))
