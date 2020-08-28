25c8da0b1bbc315615471cb0fd8d5fa35548bea0
Jakub Jaskuła <jjcreator8@gmail.com>
2020-08-28 12:23:24 +0200

    animation fix

--- app.js
+++ app.js
@@ -23,9 +23,8 @@ const mainSubheader = document.querySelector(".main__subheader");
 
 const shouldVidLoad = () => {
     if(window.innerWidth > 767) {
-        source.setAttribute("src", "video/Neon-5.webm");
+        source.setAttribute("src", "video/Neon-6.webm");
         vid.load();
-        vid.style.opacity = "0";
         vid.playbackRate = 2;
         vid.addEventListener("canplaythrough", initializeMainContent)
     }
@@ -41,11 +40,10 @@ shouldVidLoad();
 mainShowcase.addEventListener("animationend", ()=> {
     vid.removeEventListener("canplaythrough", initializeMainContent)
     nav.classList.add("slidein");
-    vid.style.opacity = "1";
 })
 
 nav.addEventListener("animationend", ()=> {
-    mainButton.style.opacity = "1"
+    mainButton.style.opacity = "1";
     mainButton.classList.add("pop");
 })
 
